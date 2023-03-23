3ed5307d0f3e2caa317de32b1441a35c52cd6328
Islam-Eleky <90136295+Islam-Eleky@users.noreply.github.com>
2023-03-23 02:13:58 +0200

    Update Sequence.sv

--- Verification/Agents/AXI_Req_Agent/Sequence.sv
+++ Verification/Agents/AXI_Req_Agent/Sequence.sv
@@ -1,26 +1,27 @@
-typedef enum bit[1:0] { FIXED, INCR, WRAP } trans_type;
-class my_sequence extends uvm_sequence #(transaction);
+typedef enum bit[1:0] { FIXED, INCR, WRAP } flit_type;
+class my_sequence extends uvm_sequence ;
+ my_transaction trans;
 `uvm_object_utils(my_sequence)
  bit [10:0]  identier;
 const int no_of_Wtrans;
-trans_type trans ;
+flit_type f_type ;
 function new(string name="my_sequence");
 super.new(name);
 endfunction
 task body();
  repeat(no_of_Wtrans) begin
     identier++;
-     trans = transaction::type_id::create("trans");
-     start_item(trans);
-        if(trans_type == 0)
-            assert(trans.randomize() with { trans_type == FIXED; });
-        else if(trans_type == 1)
-            assert(trans.randomize() with { trans_type == INCR; });
-        else if(trans_type == 2)
-            assert(trans.randomize() with { trans_type == WRAP; });
-        else
+     trans = my_transaction::type_id::create("trans");
+  start_item(trans);
+  if(f_type == 0)
+         assert(trans.randomize() with { f_type == FIXED; });
+  else if(f_type == 1)
+         assert(trans.randomize() with { f_type == INCR; });
+  else if(f_type == 2)
+         assert(trans.randomize() with { f_type == WRAP; });
+  else
             assert(trans.randomize());
-     finish_item(trans);
+  finish_item(trans);
 trans.TAG = {1'b0, identier};
  #10;
   end
