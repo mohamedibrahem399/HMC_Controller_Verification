f450b8f81c1f7cb78e838832048487bbfb49ea1b
omniasalah276 <70551793+omniasalah276@users.noreply.github.com>
2023-04-11 14:30:32 +0200

    Create AXI_Res_IF.sv

---
+++ Verification/Agents/AXI_Rsp_Agent/AXI_Res_IF.sv
@@ -0,0 +1,16 @@
+interface AXI_Res_IF #(parameter FPW = 2)(input bit clk, res);
+  
+  logic TREADY;
+  logic TVALID;
+  logic [FPW*128-1:0]TDATA; //DWIDTH = FPW*128
+  logic [FPW*16-1:0]TUSER; //NUM_DATA_BYTES = FPW*16
+
+  logic [FPW-1:0]Valid; //VALID Flags
+  logic [FPW-1:0]Hdr;   //HEADER Flags
+  logic [FPW-1:0]Tail;  //TAIL Flags
+
+  assign Valid = TUSER[FPW-1:0];
+  assign Hdr = TUSER[2*FPW-1:FPW];
+  assign Tail = TUSER[3*FPW-1:2*FPW];
+
+endinterface:AXI_Res_IF
