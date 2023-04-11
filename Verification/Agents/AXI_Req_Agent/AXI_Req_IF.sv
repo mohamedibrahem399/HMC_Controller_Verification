e6b7ed40c75bd01b33d51180520d54a3f19601ea
omniasalah276 <70551793+omniasalah276@users.noreply.github.com>
2023-04-11 14:41:41 +0200

    Update AXI_Req_IF.sv

--- Verification/Agents/AXI_Req_Agent/AXI_Req_IF.sv
+++ Verification/Agents/AXI_Req_Agent/AXI_Req_IF.sv
@@ -4,6 +4,15 @@ interface AXI_Req_IF #(parameter FPW = 2)(input clk);
   logic TVALID;
   logic [FPW*128-1:0]TDATA; // DWIDTH = FPW*128
   logic [FPW*16-1:0]TUSER; //NUM_DATA_BYTES = FPW*16
+  
+  logic [FPW-1:0]Valid; //VALID Flags
+  logic [FPW-1:0]Hdr;   //HEADER Flags
+  logic [FPW-1:0]Tail;  //TAIL Flags
+
+  assign Valid = TUSER[FPW-1:0];
+  assign Hdr = TUSER[2*FPW-1:FPW];
+  assign Tail = TUSER[3*FPW-1:2*FPW];
+	
 	
 
 
