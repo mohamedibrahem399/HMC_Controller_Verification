b5cfeeaa08f89e97270163847acbd428dc5e4e8c
omniasalah276 <70551793+omniasalah276@users.noreply.github.com>
2023-04-14 01:03:29 +0200

    Update AXI_Res_Monitor.svh

--- Verification/Agents/AXI_Rsp_Agent/AXI_Res_Monitor.svh
+++ Verification/Agents/AXI_Rsp_Agent/AXI_Res_Monitor.svh
@@ -1,10 +1,9 @@
-
-class AXI_Res_Monitor #(parameter FPW = 2) extends  uvm_monitor;
+class AXI_Res_Monitor #(parameter FPW = 4) extends  uvm_monitor;
   
   //uvm_component_utils only can register the defaults,if you intend other than the default parameter values we should use this
-  `uvm_component_param_utils(AXI_Res_Monitor #(parameter FPW = 2))
+	`uvm_component_param_utils(AXI_Res_Monitor #(parameter FPW = 4))
   
-  virtual AXI_Res_IF #(parameter FPW = 2) vif;
+  virtual AXI_Res_IF #(parameter FPW = 4) vif;
   uvm_analysis_port #(AXI_Res_Sequence_Item) Res_Mon_Port;
 
   
@@ -29,51 +28,75 @@ class AXI_Res_Monitor #(parameter FPW = 2) extends  uvm_monitor;
    task run_phase (uvm_phase phase);
      super.run_phase(phase);
      
+     AXI_Res_Sequence_Item item = AXI_Res_Sequence_Item::type_id::create("item",this);
+     int j = 1;
+
+     
+     
      forever begin
-       AXI_Res_Sequence_Item item = AXI_Res_Sequence_Item::type_id::create("item",this);
        @(posedge vif.clk);
        if (vif.TVALID == 1 && vif.TREADY == 1)begin 
-          Pin_To_Trans(item);
-          @(posedge vif.clk);
+	       Pin_To_Trans(item,j);
+       
        end
       
-       if (vif.TVALID == 0) begin
+       // I am not sure if we should rst when TVALID = 0
+       /*
+       else if (vif.TVALID == 0) begin
          //field should be zero
          To_Header(64'b0, item);
          To_Tail(64'b0, item);
-         
-       end
-       axireq_ap.write(item); 
-     end
-      
+         axireq_ap.write(item); //Where??
+       end*/
+       
+     end    
    endtask:run_phase
   
   
   
-  task Pin_To_Trans(AXI_Res_Sequence_Item item);
+  
+	task Pin_To_Trans(AXI_Res_Sequence_Item item, int j);
     
     bit[63:0] header;
-    bit[63:0] tail;
+    bit[63:0] tail;    
     
     //To access TDATA: i -> TDATA[((i+1)*128)-1 :i*128]
-    for(int i=0; i<FPW; i++)begin 
-      
-        if(vif.Valid[i])begin
-          
-           if(vif.Hdr[i])begin   
-                int Lower_Limit = i*128;
-                header = vif.TDATA[ Lower_Limit + 63 : Lower_Limit];
-                To_Header(header, item);
-            end
-              
-            if (vif.Tail[i])begin
-                int Upper_Limit = ((i+1) * 128) -1;
-                tail = vif.TDATA[ Upper_Limit : Upper_Limit - 63];
-                To_Tail(tail, item);
-            end
+    for(int i=0; i<FPW; i++)begin
+ 
+      if(vif.Valid[i])begin
+        int Upper_Limit = ((i+1) * 128) -1;
+        int Lower_Limit = i*128;
+
+        if(vif.Hdr[i])begin  
+
+          header = vif.TDATA[ Lower_Limit + 63 : Lower_Limit];
+          To_Header(header, item);
+         // fill data
+          if(item.LEN > 1) item.data[0] = vif.TDATA[ Upper_Limit : Upper_Limit - 63];
+
+        end   
+
+        if (vif.Tail[i])begin
+
+          tail = vif.TDATA[ Upper_Limit : Upper_Limit - 63];
+          To_Tail(tail, item);
+          // fill data
+          if(item.LEN > 1) item.data[$] = vif.TDATA[ Lower_Limit + 63 : Lower_Limit];
+          j = 1;
+          axireq_ap.write(item);
+          end
         
+        //fill the rest of the data
+        if(!vif.Tail[i] && !vif.Hdr[i])begin
+          data[j++] = vif.TDATA[ Lower_Limit + 63 : Lower_Limit];
+          data[j++] = vif.TDATA[ Upper_Limit : Upper_Limit - 63];
+          
         end
+        
+      end
     end
+          
+
       
   endtask:Pin_To_Trans
 	
@@ -85,12 +108,11 @@ class AXI_Res_Monitor #(parameter FPW = 2) extends  uvm_monitor;
     item.LEN = header[10:7];       
     item.DLN = header[14:11];
     item.TAG = header[23:15];
-
+	  
   endtask:To_Header
   
   
   task To_Tail (bit[63:0] tail, AXI_Res_Sequence_Item item);
-    
     item.RRP = tail[7:0];
     item.FRP = tail[15:8];
     item.SEQ = tail[18:16];
