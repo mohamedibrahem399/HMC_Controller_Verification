d64c0e74a2dd1b79e5c9992c899163e26202d752
Omnia Mohamed <110364388+OmniaMohamed12@users.noreply.github.com>
2023-04-13 05:28:19 +0200

    Update and rename AXI_Req_agent.svh to AXI_Req_Agent.svh

--- Verification/Agents/AXI_Req_Agent/AXI_Req_Agent.svh
+++ Verification/Agents/AXI_Req_Agent/AXI_Req_Agent.svh
@@ -1,29 +1,29 @@
- class AXI_Req_agent extends uvm_agent;
-    'uvm_component_utils(AXI_Req_agent)
+class AXI_Req_Agent extends uvm_agent;
+    'uvm_component_utils(AXI_Req_Agent)
 	
-    uvm_analysis_port#(Req_Packet) aport;
+    uvm_analysis_port#(AXI_Req_Sequence_Item) req_aport;
 	
 	my_sequencer sequencer;
 	AXI_Req_Driver driver;
-	axireq_moniter monitor;
+	AXI_Req_Monitor monitor;
 	AXI_Req_Config con;
 	
-	extern function new (string name="AXI_Req_agent", uvm_component parent = null);
+	extern function new (string name="AXI_Req_Agent", uvm_component parent = null);
 	extern function void build_phase (uvm_phase phase);
 	extern function void connect_phase (uvm_phase phase);
 	
- endclass: AXI_Req_agent
+ endclass: AXI_Req_Agent
 
  //////////////////////////////constructor/////////////////////////
- function AXI_Req_agent::new(string name="AXI_Req_agent", uvm_component parent = null);
+ function AXI_Req_Agent::new(string name="AXI_Req_Agent", uvm_component parent = null);
     super.new(name,parent);
  endfunction: new 
 	
  //////////////////////////////build phase/////////////////////////
- function void AXI_Req_agent::build_phase (uvm_phase phase);
+ function void AXI_Req_Agent::build_phase (uvm_phase phase);
 	super.build_phase(phase);
 		
-	aport=new("aport",this);
+	req_aport=new("req_aport",this);
 		
 	 if(!uvm_config_db#(AXI_Req_Config)::get(this,"","con",con)) 
 		'uvm_fatal("AXI_Req_Config ","failed to access AXI_Req_Config from database");
@@ -33,20 +33,20 @@
 		driver=AXI_Req_Driver::type_id::create("driver",this);
 	end
 		
-	monitor=axireq_moniter::type_id::create("monitor",this);
+	monitor=AXI_Req_Monitor::type_id::create("monitor",this);
 	con=AXI_Req_Config::type_id::create("con",this);
-	'uvm_info("AXI_Req_agent"," build phase ",UVM_HIGH)
+	'uvm_info("AXI_Req_Agent"," build phase ",UVM_HIGH)
 		
  endfunction: build_phase
 	
  ////////////////////////////connect phase/////////////////////////
- function void AXI_Req_agent::connect_phase (uvm_phase phase);
+ function void AXI_Req_Agent::connect_phase (uvm_phase phase);
 	super.connect_phase(phase);
 		
 	if(con.is_active == UVM_ACTIVE)
 		driver.seq_item_port.connect(sequencer.seq_item_export);
 		
-	monitor.aport.connect(aport);
-	'uvm_info("AXI_Req_agent"," connect phase ",UVM_HIGH)
+	monitor.axireq_ap.connect(req_aport);
+	'uvm_info("AXI_Req_Agent"," connect phase ",UVM_HIGH)
 		
  endfunction: connect_phase
