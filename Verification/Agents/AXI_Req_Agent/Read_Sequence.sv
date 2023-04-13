506c27b565ce13962694d2e44ce05e5e52c3e7e6
Islam-Eleky <90136295+Islam-Eleky@users.noreply.github.com>
2023-04-14 01:53:25 +0200

    Update Read_Sequence.sv

--- Verification/Agents/AXI_Req_Agent/Read_Sequence.sv
+++ Verification/Agents/AXI_Req_Agent/Read_Sequence.sv
@@ -1,29 +1,28 @@
 typedef enum bit[1:0] { FIXED, INCR, WRAP } flit_type;
 class my_sequence extends uvm_sequence ;
- my_transaction trans;
-`uvm_object_utils(my_sequence)
- bit [10:0]  identier;
-const int no_of_Rtrans;
-flit_type f_type ;
+   my_transaction trans;
+   `uvm_object_utils(my_sequence)
+    const int no_of_Rtrans;
+    flit_type f_type ;
 function new(string name="my_sequence");
-super.new(name);
+  super.new(name);
 endfunction
 task body();
   repeat(no_of_Rtrans) begin
-    identier++;
-     trans = my_transaction::type_id::create("trans");
+    
+  trans = my_transaction::type_id::create("trans");
   start_item(trans);
-  if(f_type == 0)
+   if(f_type == 0)
          assert(trans.randomize() with { f_type == FIXED; });
-  else if(f_type == 1)
+   else if(f_type == 1)
          assert(trans.randomize() with { f_type == INCR; });
-  else if(f_type == 2)
+   else if(f_type == 2)
          assert(trans.randomize() with { f_type == WRAP; });
-  else
-            assert(trans.randomize());
+   else
+         assert(trans.randomize());
   finish_item(trans);
-trans.TAG = {1, identier};
+
  #10;
   end
-   endtask
+endtask
 endclass
