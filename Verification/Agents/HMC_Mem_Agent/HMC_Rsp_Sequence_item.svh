b12421375fd8bf412c08eb2017ee54c96fd83791
Kholoud Ebrahim <108447715+Kholoud-Ebrahim@users.noreply.github.com>
2023-04-17 00:34:31 +0200

    HMC_Rsp_Sequence_item

--- Verification/Agents/HMC_Mem_Agent/HMC_Rsp_Sequence_item.svh
+++ Verification/Agents/HMC_Mem_Agent/HMC_Rsp_Sequence_item.svh
@@ -1,19 +1,19 @@
 `include "HMC_Mem_Types.svh"
    
-class HMC_Rsp_Sequence_item #(parameter DWIDTH = 256) extends uvm_sequence_item;
+class HMC_Rsp_Sequence_item extends uvm_sequence_item;
     //response packet header
-    rand bit [21:0] RES1;    // Reserved               [63:42]
-    rand bit [2 :0] SLID;    // Source Link ID         [41:39]
-    rand bit [5 :0] RES2;    // Reserved               [38:33] 
-    rand bit [8 :0] RTN_TAG; // Return tag (optional)  [32:24]
-    rand bit [8 :0] TAG;     // Tag number             [23:15]
-    rand bit [3 :0] DLN;     // Duplicate length       [14:11]
-    rand bit [3 :0] LNG;     // Packet length in FLITS [10:7]
-    rand bit        RES3;    // Reserved               [6]
-    rand CMD_Rsp_t  CMD;     // Packet command         [5:0]   
+    rand  bit [21:0] RES1;    // Reserved               [63:42]
+    rand  bit [2 :0] SLID;    // Source Link ID         [41:39]
+    rand  bit [5 :0] RES2;    // Reserved               [38:33] 
+    rand  bit [8 :0] RTN_TAG; // Return tag (optional)  [32:24]
+    randc bit [8 :0] TAG;     // Tag number             [23:15]
+    rand  bit [3 :0] DLN;     // Duplicate length       [14:11]
+    rand  bit [3 :0] LNG;     // Packet length in FLITS [10:7]
+    rand  bit        RES3;    // Reserved               [6]
+    rand  CMD_Rsp_t  CMD;     // Packet command         [5:0]   
 
     //response pascket data
-    rand bit [(DWIDTH/128 -1)*128 -1 : 0] data;
+    rand bit[63:0] data[$];
 
     //response packet tail
     rand bit [31:0] CRC;     // Cyclic redundancy check [63:32]
@@ -24,7 +24,7 @@ class HMC_Rsp_Sequence_item #(parameter DWIDTH = 256) extends uvm_sequence_item;
     rand bit [7 :0] FRP;     // Forward retry pointer   [15:8]
     rand bit [7 :0] RRP;     // Return retry pointer    [7:0]
 
-    `uvm_object_param_utils_begin(HMC_Rsp_Sequence_item #(DWIDTH))
+    `uvm_object_utils_begin(HMC_Rsp_Sequence_item)
         //response packet header
         `uvm_field_int(RES1,      UVM_ALL_ON)
         `uvm_field_int(SLID,      UVM_ALL_ON)
@@ -36,7 +36,7 @@ class HMC_Rsp_Sequence_item #(parameter DWIDTH = 256) extends uvm_sequence_item;
         `uvm_field_int(RES3,      UVM_ALL_ON)
         `uvm_field_enum(CMD_Rsp_t,CMD,UVM_ALL_ON)
         //response pascket data
-        `uvm_field_int(data,      UVM_ALL_ON)
+        `uvm_field_queue_int(data, UVM_ALL_ON)
         //response packet tail
         `uvm_field_int(CRC,       UVM_ALL_ON)
         `uvm_field_int(RTC,       UVM_ALL_ON)
@@ -54,7 +54,20 @@ class HMC_Rsp_Sequence_item #(parameter DWIDTH = 256) extends uvm_sequence_item;
     // constraints
     constraint c_reserved        {RES1 ==0; RES2 ==0; RES3 ==0;}
     constraint c_source_link_ID  {SLID ==0;}
-    constraint c_return_tag      {RTN_TAG ==0;}
-    constraint c_match_length    {LNG == DLN;}
+    constraint c_return_tag      {RTN_TAG ==0; LNG inside {[1:9]};}
+    constraint c_match_length    {LNG == DLN; LNG inside {[1:9]};}
+    constraint c_commands        {LNG == 1 <-> CMD inside{WR_RS, MD_WR_RS, ERROR};
+                                  LNG == 2 <-> CMD inside{RD_RS, MD_RD_RS};
+                                  LNG == 3 <-> CMD inside{RD_RS}; 
+                                  LNG == 4 <-> CMD inside{RD_RS};
+                                  LNG == 5 <-> CMD inside{RD_RS};
+                                  LNG == 6 <-> CMD inside{RD_RS};
+                                  LNG == 7 <-> CMD inside{RD_RS};
+                                  LNG == 8 <-> CMD inside{RD_RS};
+                                  LNG == 9 <-> CMD inside{RD_RS};}
+    constraint c_data{  if(LNG <= 2)
+                            data.size == (LNG-1)*2;
+                        else 
+                            data.size == (LNG-2)*2 + 2;}
 
 endclass: HMC_Rsp_Sequence_item
