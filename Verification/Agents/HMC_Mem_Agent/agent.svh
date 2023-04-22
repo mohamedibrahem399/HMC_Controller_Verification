4e7ea28e727122f2bb6c95028028a45798fc432b
Mohamed Ibrahem <93216929+mohamedibrahem399@users.noreply.github.com>
2023-04-22 03:52:16 +0200

    Update agent.svh

--- Verification/Agents/HMC_Mem_Agent/agent.svh
+++ Verification/Agents/HMC_Mem_Agent/agent.svh
@@ -1,62 +1,48 @@
-/*
-`include "seq_item.sv"
-`include "sequencer.sv"
-`include "sequence.sv"
-`include "driver.sv"
-`include "monitor.sv"
-*/
-
 class RA_agent extends uvm_agent;
 
-  //---------------------------------------
-  // component instances
-  //---------------------------------------
+    //---------------------------------------
+    // component instances
+    //---------------------------------------
+    HMC_Mem_Driver    driver;
+    HMC_Mem_Sequencer sequencer;
+    RA_monitor   monitor;
+    HMC_Mem_Storage  hmc_memory;
+    `uvm_component_utils(RA_agent)
 
-  RA_driver    driver;
-  RA_sequencer sequencer;
-  RA_monitor   monitor;
-  HMC_Mem_Storage  hmc_memory;
-  `uvm_component_utils(RA_agent)
+    //---------------------------------------
+    // constructor
+    //---------------------------------------
+    function new (string name= "RA_agent", uvm_component parent);
+      super.new(name, parent);
+    endfunction : new
 
-  //---------------------------------------
-  // constructor
-  //---------------------------------------
-
-  function new (string name, uvm_component parent);
-    super.new(name, parent);
-  endfunction : new
-
-  //---------------------------------------
-  // build_phase
-  //---------------------------------------
-
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-
-    
-    monitor = RA_monitor::type_id::create("monitor", this);
-
-    //creating driver and sequencer only for ACTIVE agent
-    if(get_is_active() == UVM_ACTIVE) begin
-      driver    = RA_driver::type_id::create("driver", this);
-      sequencer = RA_sequencer::type_id::create("sequencer", this);
-      monitor    = RA_monitor::type_id::create("monitor", this);
-      hmc_memory   = HMC_Mem_Storage::type_id::create("hmc_memory", this);
-    end
-    
-
-  endfunction : build_phase
+    //---------------------------------------
+    // build_phase
+    //---------------------------------------
+    function void build_phase(uvm_phase phase);
+      super.build_phase(phase);
+      `uvm_info("HMC_Agent","build_phase", UVM_HIGH)
   
-  //---------------------------------------  
-  // connect_phase - connecting the driver and sequencer port
-  //---------------------------------------
-  function void connect_phase(uvm_phase phase);
-
-    if(get_is_active() == UVM_ACTIVE) begin
-      driver.RA_seq_item_port.connect(sequencer.RA_seq_item_export);
-      monitor.mem_put_port.connect(hmc_memory.mem_put_imp);
-    end
-
-  endfunction : connect_phase
+      //creating driver, sequencer, monitor and memorty 
+      if(get_is_active() == UVM_ACTIVE) begin
+        driver    = HMC_Mem_Driver::type_id::create("driver", this);
+        sequencer = RA_sequencer::type_id::create("sequencer", this);
+        monitor    = RA_monitor::type_id::create("monitor", this);
+        hmc_memory   = HMC_Mem_Storage::type_id::create("hmc_memory", this);
+      end
+    endfunction : build_phase
+  
+    //---------------------------------------  
+    // connect_phase - connecting the driver and sequencer port
+    //---------------------------------------
+    function void connect_phase(uvm_phase phase);
+      `uvm_info("HMC_Agent","connect_phase", UVM_HIGH)
+      if(get_is_active() == UVM_ACTIVE) begin
+        driver.RA_seq_item_port.connect(sequencer.RA_seq_item_export);
+        monitor.mem_put_port.connect(hmc_memory.HMC_Mem_Analysis_Monitor_Storage_Imp);
+        driver.seq_item_port.connect(sequencer.seq_item_export);
+        hmc_memory.HMC_Mem_Analysis_Storage_Sequencer_Port.connect(sequencer.HMC_Mem_Analysis_Storage_Sequencer_Export);
+      end
+    endfunction : connect_phase
 
 endclass : RA_agent
