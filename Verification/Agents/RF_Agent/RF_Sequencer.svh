383072f518997c3d74d1a0485e5d6bab77274624
Kholoud Ebrahim <108447715+Kholoud-Ebrahim@users.noreply.github.com>
2023-05-04 03:07:49 +0300

    RF_Sequencer_update

--- Verification/Agents/RF_Agent/RF_Sequencer.svh
+++ Verification/Agents/RF_Agent/RF_Sequencer.svh
@@ -4,19 +4,19 @@ class RF_Sequencer extends uvm_sequencer #(RF_Sequence_Item);
     //Constructor
     function new(string name = "RF_Sequencer", uvm_component parent);
         super.new(name, parent);
-        `uvm_info("SEQR_CLASS", "Inside Constructor!", UVM_HIGH)
+        `uvm_info(get_type_name(), $sformatf("%m"), UVM_NONE)
       endfunction: new
 
     //Build Phase
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        `uvm_info("SEQR_CLASS", "Build Phase!", UVM_HIGH)
+        `uvm_info(get_type_name(), $sformatf("%m"), UVM_NONE)
     endfunction: build_phase
 
     //Connect Phase
     function void connect_phase(uvm_phase phase);
         super.connect_phase(phase);
-        `uvm_info("SEQR_CLASS", "Connect Phase!", UVM_HIGH)
+        `uvm_info(get_type_name(), $sformatf("%m"), UVM_NONE)
     endfunction: connect_phase
 
 endclass: RF_Sequencer
