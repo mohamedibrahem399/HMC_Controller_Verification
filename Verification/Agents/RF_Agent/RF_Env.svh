2e708ae87b6c827b626d4a5b49153d473472ad8a
Kholoud Ebrahim <108447715+Kholoud-Ebrahim@users.noreply.github.com>
2023-05-04 03:03:27 +0300

    RF_Env_update

--- Verification/Agents/RF_Agent/RF_Env.svh
+++ Verification/Agents/RF_Agent/RF_Env.svh
@@ -1,29 +1,27 @@
-class RF_Env#(parameter HMC_RF_WWIDTH= 64, HMC_RF_RWIDTH= 64, HMC_RF_AWIDTH= 4) extends uvm_env;
-    `uvm_component_param_utils(RF_Env#(HMC_RF_WWIDTH, HMC_RF_RWIDTH, HMC_RF_RWIDTH))
+class RF_Env extends uvm_env;
+    `uvm_component_param_utils(RF_Env)
     RF_Agent agnt;
     RF_Scoreboard scb;
-    
-        
+     
     function new(string name = "RF_Env", uvm_component parent);
         super.new(name, parent);
-        `uvm_info("ENV_CLASS", "Inside Constructor!", UVM_HIGH)
+        `uvm_info(get_type_name(), $sformatf("%m"), UVM_NONE)
     endfunction: new
   
 
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    `uvm_info("ENV_CLASS", "Build Phase!", UVM_HIGH)
+    `uvm_info(get_type_name(), $sformatf("%m"), UVM_NONE)
     
-    agnt = RF_Agent#(HMC_RF_WWIDTH, HMC_RF_RWIDTH, HMC_RF_AWIDTH)::type_id::create("agnt", this);
+    agnt = RF_Agent::type_id::create("agnt", this);
     scb = RF_Scoreboard::type_id::create("scb", this);
   endfunction: build_phase
   
-  
   function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
-    `uvm_info("ENV_CLASS", "Connect Phase!", UVM_HIGH)
+    `uvm_info(get_type_name(), $sformatf("%m"), UVM_NONE)
     
-    agnt.mon.monitor_port.connect(scb.scoreboard_port);
+    agnt.mon.monitor_port.connect(scb.scoreboard_imp);
   endfunction: connect_phase
   
 endclass: RF_Env
