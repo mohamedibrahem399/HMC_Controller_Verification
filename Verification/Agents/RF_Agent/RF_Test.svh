fba20e39925654b7f63931d23e31fa55b4cb83e5
Kholoud Ebrahim <108447715+Kholoud-Ebrahim@users.noreply.github.com>
2023-05-04 03:10:23 +0300

    RF_Test_update

--- Verification/Agents/RF_Agent/RF_Test.svh
+++ Verification/Agents/RF_Agent/RF_Test.svh
@@ -1,38 +1,37 @@
-class RF_Test #(parameter HMC_RF_WWIDTH= 64, HMC_RF_RWIDTH= 64, HMC_RF_AWIDTH= 4)extends uvm_test;
-    //`uvm_component_param_utils(RF_Test#(HMC_RF_WWIDTH, HMC_RF_RWIDTH, HMC_RF_RWIDTH))
-    `uvm_component_registry(RF_Test#(HMC_RF_WWIDTH, HMC_RF_RWIDTH, HMC_RF_RWIDTH), "RF_Test")
+class RF_Test extends uvm_test;
+    `uvm_component_utils(RF_Test)
 
     RF_Env env;
-    RF_Sequence seq;
+    RF_Read_Control_Sequence seq;
 
     function new(string name = "RF_Test", uvm_component parent);
         super.new(name, parent);
-        `uvm_info("TEST_CLASS", "Inside Constructor!", UVM_HIGH)
+        `uvm_info(get_type_name(), $sformatf("%m"), UVM_NONE)
     endfunction: new
     
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        `uvm_info("TEST_CLASS", "Build Phase!", UVM_HIGH)
+        `uvm_info(get_type_name(), $sformatf("%m"), UVM_NONE)
     
-        env = RF_Env#(HMC_RF_WWIDTH, HMC_RF_RWIDTH, HMC_RF_AWIDTH)::type_id::create("env", this);
+        env = RF_Env::type_id::create("env", this);
     endfunction: build_phase
 
     function void connect_phase(uvm_phase phase);
         super.connect_phase(phase);
-        `uvm_info("TEST_CLASS", "Connect Phase!", UVM_HIGH)
+        `uvm_info(get_type_name(), $sformatf("%m"), UVM_NONE)
     endfunction: connect_phase
 
     task run_phase (uvm_phase phase);
         super.run_phase(phase);
-        `uvm_info("TEST_CLASS", "Run Phase!", UVM_HIGH)
+        `uvm_info(get_type_name(), $sformatf("%m"), UVM_NONE)
     
         phase.raise_objection(this);
 
-        repeat(500) begin
+      forever begin
           //seq
-          seq = RF_Sequence#(HMC_RF_WWIDTH, HMC_RF_RWIDTH, HMC_RF_AWIDTH)::type_id::create("seq");
-          seq.start(env.agnt.sqr); // call body method of sequence
-        end
+          //seq = RF_Read_Control_Sequence::type_id::create("seq");
+          //seq.start(env.agnt.sqr); // call body method of sequence
+      end
 
         phase.drop_objection(this);
     
