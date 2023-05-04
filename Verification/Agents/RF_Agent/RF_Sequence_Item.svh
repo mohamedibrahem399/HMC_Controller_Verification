6afaf00eb9abd8128563fc9da04a149ec3e51ee4
Kholoud Ebrahim <108447715+Kholoud-Ebrahim@users.noreply.github.com>
2023-05-04 03:07:08 +0300

    RF_Sequence_Item_update

--- Verification/Agents/RF_Agent/RF_Sequence_Item.svh
+++ Verification/Agents/RF_Agent/RF_Sequence_Item.svh
@@ -1,10 +1,10 @@
-class RF_Sequence_Item #(parameter HMC_RF_WWIDTH= 64, HMC_RF_RWIDTH= 64, HMC_RF_AWIDTH= 4) extends uvm_sequence_item;
-    rand bit [HMC_RF_WWIDTH-1:0] rf_write_data;
-    rand bit [HMC_RF_AWIDTH-1:0] rf_address;
+class RF_Sequence_Item extends uvm_sequence_item;
+    rand bit [63:0] rf_write_data;
+    rand bit [3:0] rf_address;
     rand bit rf_write_en;
     rand bit rf_read_en;
 
-    bit [HMC_RF_RWIDTH-1:0]rf_read_data;
+    bit [63:0]rf_read_data;
     bit rf_invalid_address;
     bit rf_access_complete;
 
@@ -13,10 +13,15 @@ class RF_Sequence_Item #(parameter HMC_RF_WWIDTH= 64, HMC_RF_RWIDTH= 64, HMC_RF_
         `uvm_field_int(rf_address   , UVM_ALL_ON)
         `uvm_field_int(rf_write_en  , UVM_ALL_ON)
         `uvm_field_int(rf_read_en   , UVM_ALL_ON)
+        
+        `uvm_field_int(rf_read_data   , UVM_ALL_ON)
+        `uvm_field_int(rf_invalid_address   , UVM_ALL_ON)
+        `uvm_field_int(rf_access_complete   , UVM_ALL_ON)
     `uvm_object_utils_end
     
     function new(string name= "RF_Sequence_Item");
         super.new(name);
+        `uvm_info(get_type_name(), $sformatf("%m"), UVM_NONE)
     endfunction: new
 
 endclass: RF_Sequence_Item
