020d1226f5d45bed16268de4d79054f98fc71709
Omnia Mohamed <110364388+OmniaMohamed12@users.noreply.github.com>
2023-05-05 19:01:35 +0300

    Update AXI_Res_Driver_.svh

--- Verification/Agents/AXI_Rsp_Agent/AXI_Res_Driver_.svh
+++ Verification/Agents/AXI_Rsp_Agent/AXI_Res_Driver_.svh
@@ -1,42 +1,47 @@
- class AXI_Res_Driver extends uvm_driver#(AXI_Res_SequenceItem);
-   'uvm_component_utils(AXI_Res_Driver)
+class AXI_Rsp_Driver#(parameter FPW = 4) extends uvm_driver#(AXI_Rsp_Sequence_Item);
+  `uvm_component_param_utils(AXI_Rsp_Driver#(FPW))
    
-	virtual AXI_Res_IF#(parameter FPW = 4) VIF;
-	AXI_Res_SequenceItem res_seq_item;
-	extern function new (string name="AXI_Res_Driver", uvm_component parent = null);
+	virtual AXI_Rsp_IF#(FPW) VIF;
+	AXI_Rsp_Sequence_Item rsp_seq_item;
+	
+    extern function new (string name="AXI_Rsp_Driver", uvm_component parent = null);
 	extern  function void build_phase (uvm_phase phase);
 	extern  task run_phase (uvm_phase phase);
 	extern  task drive_item();
- endclass: AXI_Response_Driver
+	
+ endclass: AXI_Rsp_Driver
 
  ////////////////////////////constructor/////////////////////////
- function AXI_Res_Driver::new(string name="AXI_Res_Driver", uvm_component parent = null);
+ function AXI_Rsp_Driver::new(string name="AXI_Rsp_Driver", uvm_component parent = null);
 	super.new(name,parent);
  endfunction: new 
 	
  ////////////////////////////build phase///////////////////////
- function void AXI_Res_Driver::build_phase (uvm_phase phase);
+ function void AXI_Rsp_Driver::build_phase (uvm_phase phase);
 	super.build_phase(phase);
-	if(!uvm_config_db#(virtual AXI_Res_IF)::get(this,"","AXI_Res_VIF",VIF))
-	    'uvm_fatal("AXI_Res_Driver ","failed to access AXI_Res_VIF from database");
+   if(!uvm_config_db#(virtual AXI_Rsp_IF)::get(this,"","AXI_Rsp_VIF",VIF))
+      `uvm_fatal("AXI_Rsp_Driver ","failed to access AXI_Rsp_VIF from database");
 		
-	'uvm_info("AXI_Res_Driver"," build phase ",UVM_HIGH)
+   `uvm_info("AXI_Rsp_Driver"," build phase ",UVM_HIGH)
 		
  endfunction: build_phase
 	
  /////////////////////////run phase////////////////////////
- task AXI_Res_Driver::run_phase (uvm_phase phase);
+ task AXI_Rsp_Driver::run_phase (uvm_phase phase);
 	super.run_phase(phase);
-	'uvm_info("AXI_Res_Driver"," run phase ",UVM_HIGH)
+   `uvm_info("AXI_Rsp_Driver"," run phase ",UVM_HIGH)
+   
     forever begin 
-	    res_seq_item=AXI_Res_SequenceItem::type_id::create("res_seq_item");
-	    seq_item_port.get_next_item(res_seq_item);
+      rsp_seq_item=AXI_Rsp_Sequence_Item::type_id::create("rsp_seq_item");
+      seq_item_port.get_next_item(rsp_seq_item);
 	        drive_item();
-	    seq_item_port.item_done();
+	  seq_item_port.item_done();
 	end
+	
  endtask: run_phase
   ///////////////////////drive_item///////////////////////
- task AXI_Res_Driver:: drive_item();
+ task AXI_Rsp_Driver:: drive_item();
+ 
 	@(posedge VIF.clk);
 	if(!VIF.rst) begin 
         VIF.TREADY<=0;
@@ -45,7 +50,7 @@
 	    end
 	else begin 
 		if(VIF.TVALID)begin 
-		    VIF.TREADY<=res_seq_item.TREADY;
+		    VIF.TREADY<=rsp_seq_item.TREADY;
 		end 
 		else begin 
 		   @(posedge VIF.clk);
@@ -53,4 +58,5 @@
 		   
 		end
 	end
+	
  endtask:drive_item
