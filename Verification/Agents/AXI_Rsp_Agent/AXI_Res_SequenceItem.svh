47cd16f0cdd2f240d6cf3157706ecc994603795c
Omnia Mohamed <110364388+OmniaMohamed12@users.noreply.github.com>
2023-05-05 18:54:21 +0300

    Update AXI_Res_SequenceItem.svh

--- Verification/Agents/AXI_Rsp_Agent/AXI_Res_SequenceItem.svh
+++ Verification/Agents/AXI_Rsp_Agent/AXI_Res_SequenceItem.svh
@@ -1,13 +1,19 @@
-class AXI_Res_SequenceItem extends uvm_sequence_item;
+class AXI_Rsp_Sequence_Item extends uvm_sequence_item;
    
-   `uvm_object_utils(AXI_Res_SequenceItem)
    rand bit TREADY;
-   
+	
+     `uvm_object_utils_begin(AXI_Rsp_Sequence_Item)
+	   `uvm_field_int(TREADY, UVM_DEFAULT  | UVM_DEC)
+     `uvm_object_utils_end
+	
    constraint RX_TREADY{
         TREADY dist {0:=20, 1:=80};}
-//////////////////////////////constructor/////////////////////////
-    function new(string name="AXI_Res_Sequence_Item");
-       super.new(name);
-    endfunction: new
 	
- endclass: AXI_Res_SequenceItem
+//////////////////////////////constructor/////////////////////////
+  function new(string name = "AXI_Rsp_Sequence_Item");
+		super.new(name);
+  endfunction
+	
+ 
+  
+endclass: AXI_Rsp_Sequence_Item
