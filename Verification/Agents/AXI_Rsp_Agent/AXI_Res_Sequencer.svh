b588c40ac9d35e733a679fc8f412f40f51e0560d
Omnia Mohamed <110364388+OmniaMohamed12@users.noreply.github.com>
2023-05-05 19:00:09 +0300

    Update AXI_Res_Sequencer.svh

--- Verification/Agents/AXI_Rsp_Agent/AXI_Res_Sequencer.svh
+++ Verification/Agents/AXI_Rsp_Agent/AXI_Res_Sequencer.svh
@@ -1,9 +1,11 @@
-class AXI_Res_Sequencer extends uvm_sequencer;
+class AXI_Rsp_Sequencer extends uvm_sequencer;
    
-   `uvm_component_utils(AXI_Res_Sequencer)
+  `uvm_component_utils(AXI_Rsp_Sequencer)
+  
    ////////////////////////////constructor/////////////////////////
-    function new(string name="AXI_Res_Sequencer", uvm_component parent = null);
+   
+  function new(string name="AXI_Rsp_Sequencer", uvm_component parent = null);
 	    super.new(name,parent);
     endfunction: new 
 
-endclass: AXI_Res_Sequencer
+endclass: AXI_Rsp_Sequencer
