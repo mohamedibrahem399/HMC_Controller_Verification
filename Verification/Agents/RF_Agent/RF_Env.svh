02f2dc05947f174c754eb140b7564bf12910cd13
Kholoud Ebrahim <108447715+Kholoud-Ebrahim@users.noreply.github.com>
2023-05-18 05:14:33 +0300

    RF_Env_update

--- Verification/Agents/RF_Agent/RF_Env.svh
+++ Verification/Agents/RF_Agent/RF_Env.svh
@@ -1,27 +1,23 @@
 class RF_Env extends uvm_env;
     `uvm_component_param_utils(RF_Env)
-    RF_Agent agnt;
-    RF_Scoreboard scb;
-     
+
+    RF_Agent  agnt;
+
     function new(string name = "RF_Env", uvm_component parent);
         super.new(name, parent);
-        `uvm_info(get_type_name(), $sformatf("%m"), UVM_NONE)
+        `uvm_info(get_type_name(), "Inside Constructor!", UVM_NONE)
     endfunction: new
-  
 
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    `uvm_info(get_type_name(), $sformatf("%m"), UVM_NONE)
-    
-    agnt = RF_Agent::type_id::create("agnt", this);
-    scb = RF_Scoreboard::type_id::create("scb", this);
-  endfunction: build_phase
-  
-  function void connect_phase(uvm_phase phase);
-    super.connect_phase(phase);
-    `uvm_info(get_type_name(), $sformatf("%m"), UVM_NONE)
-    
-    agnt.mon.monitor_port.connect(scb.scoreboard_imp);
-  endfunction: connect_phase
-  
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        `uvm_info(get_type_name(), "Build Phase!", UVM_NONE)
+        
+        agnt = RF_Agent::type_id::create("agnt", this);
+    endfunction: build_phase
+
+    function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+        `uvm_info(get_type_name(), "Connect Phase!", UVM_NONE)
+        
+    endfunction: connect_phase
 endclass: RF_Env
