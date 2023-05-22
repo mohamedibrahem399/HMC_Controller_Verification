6c875b5b4bb46fdab311f6dae605b9aeb20b12af
Omnia Mohamed <110364388+OmniaMohamed12@users.noreply.github.com>
2023-05-23 01:30:47 +0300

    Update Sys_Sequence.svh

--- Verification/Agents/Sys_Agent/Sys_Sequence.svh
+++ Verification/Agents/Sys_Agent/Sys_Sequence.svh
@@ -1,6 +1,7 @@
 class Sys_Sequence extends uvm_sequence;
   `uvm_object_utils(Sys_Sequence)
    Sys_Sequence_Item  seq_item;
+	
   function new (string name = "Sys_Sequence");
          super.new(name);
      endfunction: new
@@ -9,7 +10,12 @@ class Sys_Sequence extends uvm_sequence;
 
       seq_item=Sys_Sequence_Item::type_id::create("seq_item",this);
       start_item(seq_item);
-      assert(seq_item.randomize());
+	  
+       seq_item.clk_user=1;
+       seq_item.clk_hmc=1;
+       seq_item.res_n_user=0;
+       seq_item.res_n_hmc=0;
+	   
       finish_item(seq_item);
       
 	endtask: body
