ae3f1f5e5dbf9d40cae7600066d618e55030955f
Omnia Mohamed <110364388+OmniaMohamed12@users.noreply.github.com>
2023-07-06 19:21:41 +0300

    Update AXI_Rsp_Sequence.svh

--- Verification/Agents/AXI_Rsp_Agent/AXI_Rsp_Sequence.svh
+++ Verification/Agents/AXI_Rsp_Agent/AXI_Rsp_Sequence.svh
@@ -1,18 +1,19 @@
 
 class AXI_Rsp_Sequence extends uvm_sequence;
   `uvm_object_utils(AXI_Rsp_Sequence)
-   AXI_Rsp_Sequence_Item  rsp_seq_item;
+ 
+   AXI_Rsp_Sequence_Item  axi_rsp_item;
+   
   function new (string name = "AXI_Rsp_Sequence");
-         super.new(name);
-     endfunction: new
-	 
-	task body;
-
-      rsp_seq_item=AXI_Rsp_Sequence_Item::type_id::create("rsp_seq_item",this);
-      start_item(rsp_seq_item);
-      assert(rsp_seq_item.randomize());
-      finish_item(rsp_seq_item);
-      
-	endtask: body
+    super.new(name);
+  endfunction: new
+  
+  task body();
+	axi_rsp_item = AXI_Rsp_Sequence_Item::type_id::create("axi_rsp_item");
+	start_item(axi_rsp_item);
+	assert(axi_rsp_item.randomize());
+	finish_item(axi_rsp_item);  
+  endtask: body
 	
 endclass: AXI_Rsp_Sequence
+
