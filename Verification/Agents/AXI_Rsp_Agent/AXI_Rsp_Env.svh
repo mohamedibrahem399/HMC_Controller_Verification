c7db5e6831315a10eb80f988b8d7fd139465ee48
Omnia Mohamed <110364388+OmniaMohamed12@users.noreply.github.com>
2023-07-06 19:20:14 +0300

    Update AXI_Rsp_Env.svh

--- Verification/Agents/AXI_Rsp_Agent/AXI_Rsp_Env.svh
+++ Verification/Agents/AXI_Rsp_Agent/AXI_Rsp_Env.svh
@@ -1,42 +1,43 @@
- class AXI_Rsp_Env#(parameter FPW = 4) extends uvm_env;
-    `uvm_component_param_utils(AXI_Rsp_Env#(FPW))
+ class AXI_Rsp_Env extends uvm_env;
+  `uvm_component_param_utils(AXI_Rsp_Env)
 	
-	virtual AXI_Rsp_IF Rsp_VIF;
-	AXI_Rsp_Agent Rsp_agent;
-	AXI_Rsp_Scoreboard scoreboard;
+	AXI_Rsp_Agent       axi_rsp_agnt;
+	AXI_Rsp_Scoreboard  axi_rsp_scb;
+	AXI_Rsp_Subscriber  axi_rsp_cov;
 	
     extern function new (string name="AXI_Rsp_Env", uvm_component parent = null);
 	extern function void build_phase (uvm_phase phase);
 	extern function void connect_phase (uvm_phase phase);
 	
+ endclass:AXI_Rsp_Env
 	
-endclass:AXI_Rsp_Env
-
- //////////////////////////////constructor/////////////////////////
- function AXI_Rsp_Env::new(string name="AXI_Rsp_Env", uvm_component parent = null);
-    super.new(name,parent);
- endfunction: new 
-	
- //////////////////////////////build phase/////////////////////////
- function void AXI_Rsp_Env::build_phase (uvm_phase phase);
-	super.build_phase(phase);
-	
-   if(!uvm_config_db#(virtual AXI_Rsp_IF)::get(this,"","AXI_Rsp_VIF",Rsp_VIF))
-     `uvm_fatal("AXI_Res_Env ","failed to access AXI_Rsp_VIF from database");		
-
-	Rsp_agent=AXI_Rsp_Agent#(FPW)::type_id::create("Res_agent",this);
-	scoreboard=AXI_Rsp_Scoreboard::type_id::create("scoreboard",this);
-	
-   `uvm_info("AXI_Rsp_Env"," build phase ",UVM_HIGH)
+	//////////////////////////////constructor/////////////////////////
+	function AXI_Rsp_Env::new(string name="AXI_Rsp_Env", uvm_component parent=null);
+		super.new(name,parent);
 		
- endfunction: build_phase
-	
- ////////////////////////////connect phase/////////////////////////
- function void AXI_Rsp_Env::connect_phase (uvm_phase phase);
-	super.connect_phase(phase);
-
-   Rsp_agent.monitor.Rsp_Mon_Port.connect(scoreboard.Rsp_aport);
-	
-   `uvm_info("AXI_Rsp_Env"," connect phase ",UVM_HIGH)
+	endfunction: new 
+ 
+  //////////////////////////////build phase/////////////////////////
+	function void AXI_Rsp_Env::build_phase (uvm_phase phase);
+		super.build_phase(phase);
 		
- endfunction: connect_phase
+		axi_rsp_agnt=AXI_Rsp_Agent::type_id::create("axi_rsp_agnt",this);
+		axi_rsp_scb=AXI_Rsp_Scoreboard::type_id::create("axi_rsp_scb",this);
+		axi_rsp_cov=AXI_Rsp_Subscriber::type_id::create("axi_rsp_cov",this);
+		
+		`uvm_info("AXI_Rsp_Env"," build phase ",UVM_HIGH)
+			
+	endfunction: build_phase
+	
+	////////////////////////////connect phase/////////////////////////
+	function void AXI_Rsp_Env::connect_phase (uvm_phase phase);
+		super.connect_phase(phase);
+		
+	   axi_rsp_agnt.axi_rsp_mon.Rsp_Mon_Port.connect(axi_rsp_scb.Rsp_aport);
+	   axi_rsp_agnt.axi_rsp_mon.Rsp_Mon_Port.connect(axi_rsp_cov.axi_rsp_imp);
+		
+	    `uvm_info("AXI_Rsp_Env"," connect phase ",UVM_HIGH)
+			
+	endfunction: connect_phase
+
+ 
