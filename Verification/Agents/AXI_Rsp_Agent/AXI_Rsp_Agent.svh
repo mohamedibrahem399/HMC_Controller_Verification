44da6c43207b87e0bbb25130625c6414a2c46b75
Omnia Mohamed <110364388+OmniaMohamed12@users.noreply.github.com>
2023-07-06 19:15:03 +0300

    Update AXI_Rsp_Agent.svh

--- Verification/Agents/AXI_Rsp_Agent/AXI_Rsp_Agent.svh
+++ Verification/Agents/AXI_Rsp_Agent/AXI_Rsp_Agent.svh
@@ -1,43 +1,41 @@
-class AXI_Rsp_Agent#(parameter FPW = 4) extends uvm_agent;
-    `uvm_component_param_utils(AXI_Rsp_Agent#(FPW))
-	
-
-	
-	AXI_Rsp_Sequencer sequencer;
-	AXI_Rsp_Driver#(FPW) driver;
-	AXI_Rsp_Monitor#(FPW) monitor;
+class AXI_Rsp_Agent extends uvm_agent;
+    `uvm_component_utils(AXI_Rsp_Agent)
+  
+	AXI_Rsp_Sequencer   axi_rsp_sqr;
+	AXI_Rsp_Driver   	axi_rsp_drv;
+	AXI_Rsp_Monitor     axi_rsp_mon;
+    
 	
 	extern function new (string name="AXI_Rsp_Agent", uvm_component parent = null);
 	extern function void build_phase (uvm_phase phase);
 	extern function void connect_phase (uvm_phase phase);
 	
- endclass: AXI_Rsp_Agent
-
- //////////////////////////////constructor/////////////////////////
- function AXI_Rsp_Agent::new(string name="AXI_Rsp_Agent", uvm_component parent = null);
-    super.new(name,parent);
-	
- endfunction: new 
-	
- //////////////////////////////build phase/////////////////////////
- function void AXI_Rsp_Agent::build_phase (uvm_phase phase);
-	super.build_phase(phase);
-		
+ endclass: AXI_Rsp_Agent 
  
-	sequencer=AXI_Rsp_Sequencer::type_id::create("sequencer",this);
-	driver=AXI_Rsp_Driver#(FPW)::type_id::create("driver",this);
-	monitor=AXI_Rsp_Monitor#(FPW)::type_id::create("monitor",this);
-	
+ //////////////////////////////constructor///////////////////////// 
+ function AXI_Rsp_Agent::new(string name="AXI_Rsp_Agent", uvm_component parent = null);
+	super.new(name,parent);
+endfunction: new 
+
+ //////////////////////////////build phase///////////////////////// 
+function void AXI_Rsp_Agent::build_phase (uvm_phase phase);
+	super.build_phase(phase);
+
 	`uvm_info("AXI_Rsp_Agent"," build phase ",UVM_HIGH)
-		
- endfunction: build_phase
-	
- ////////////////////////////connect phase/////////////////////////
- function void AXI_Rsp_Agent::connect_phase (uvm_phase phase);
+
+	axi_rsp_sqr = AXI_Rsp_Sequencer::type_id::create("axi_rsp_sqr",this);
+	axi_rsp_drv = AXI_Rsp_Driver::type_id::create("axi_rsp_drv",this);
+	axi_rsp_mon = AXI_Rsp_Monitor::type_id::create("axi_rsp_mon",this);
+			
+endfunction: build_phase
+
+ ////////////////////////////connect phase///////////////////////// 
+function void AXI_Rsp_Agent::connect_phase (uvm_phase phase);
 	super.connect_phase(phase);
-	
-	driver.seq_item_port.connect(sequencer.seq_item_export);
-	
-	`uvm_info("AXI_Rsp_Agent"," connect phase ",UVM_HIGH)
 		
- endfunction: connect_phase
+	`uvm_info("AXI_Rsp_Agent"," connect phase ",UVM_HIGH)	
+		
+	axi_rsp_drv.seq_item_port.connect(axi_rsp_sqr.seq_item_export);	
+			
+endfunction: connect_phase
+
