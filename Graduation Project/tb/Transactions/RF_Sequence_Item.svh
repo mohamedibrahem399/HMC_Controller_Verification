05a14f785fd4b847052116f73541ae8530ecc980
Kholoud Ebrahim <108447715+Kholoud-Ebrahim@users.noreply.github.com>
2023-07-14 00:23:03 +0300

    RF_Sequence_Item

---
+++ Graduation Project/tb/Transactions/RF_Sequence_Item.svh
@@ -0,0 +1,25 @@
+class RF_Sequence_Item extends uvm_sequence_item;
+    rand bit [63:0] rf_write_data;
+    rand bit [3:0] rf_address;
+    rand bit rf_write_en;
+    rand bit rf_read_en;
+
+    bit [63:0]rf_read_data;
+    bit rf_invalid_address;
+    bit rf_access_complete;
+
+    `uvm_object_utils_begin(RF_Sequence_Item)
+        `uvm_field_int(rf_write_data, UVM_ALL_ON)
+        `uvm_field_int(rf_address   , UVM_ALL_ON)
+        `uvm_field_int(rf_write_en  , UVM_ALL_ON)
+        `uvm_field_int(rf_read_en   , UVM_ALL_ON)
+        `uvm_field_int(rf_read_data   , UVM_ALL_ON)
+        `uvm_field_int(rf_invalid_address   , UVM_ALL_ON)
+        `uvm_field_int(rf_access_complete   , UVM_ALL_ON)
+    `uvm_object_utils_end
+    
+    function new(string name= "RF_Sequence_Item");
+        super.new(name);
+        `uvm_info(get_type_name(), "Inside Constructor!", UVM_NONE)
+    endfunction: new
+endclass: RF_Sequence_Item
